982128c1edc548747a6b5c7216186e38fe00e1d5
Alejandro <alejandro@liqi.it>
2023-06-26 16:46:13 +0200

    first outline of the package. TODO-Models, then Builders, afterthat plots and tables themes

---
+++ R/utils.R
@@ -0,0 +1,22 @@
+#' Check Date type
+#'
+#' @description
+#' TRUE is all elements of the vector are Date type, FALSE otherwise.
+#'
+#' @param vec A vector or string.
+#' @returns A Boolean
+#' @examples
+#' non_dates = c('2020-01-01', as.Date('2021-02-02'))
+#' is.Date(non_dates)
+#' ok_dates = as.Date(c('2020-01-01', '2021-02-02'))
+#' is.Date(ok_dates)
+
+is.Date = function(vec) {
+
+    if (!is.vector(vec)) {
+        stop("Error: elements must be contained in a vector.")
+    }
+
+    inherits(vec, "Date")
+
+}
