8da8008a9a2e2b4d35100da8189bbc3cfec2a488
Alejandro <alejandro@liqi.it>
2023-08-29 14:38:53 +0200

    new functions for querying and sql

--- R/s3.R
+++ R/s3.R
@@ -1,8 +1,11 @@
+
+
 #' AWS S3 Bucket Download client's file
 #'
 #' Get a file from the client's directory in the S3 Risk bucket
 #'
 #' @param object_path the object's full path in the S3 bucket
+#' @param dest_path the local path
 #' @param company_path the bucket's S3 internal path of the company
 #' @param complete recreate the bucket path locally
 #' @param risk_bucket the aws bucket
@@ -62,45 +65,183 @@ s3_retrieve_object = function(object_path, company_path, dest_path = 'clients', 
 #' @importFrom aws.s3 save_object
 #'
 
-s3_retrieve_directory = function(company, date, dest_path, data_type, risk_bucket = "s3://risk-clientele/clients", zipped = FALSE) {
+s3_retrieve_directory = function(company, date, dest_path = NULL, data_type = 'full', risk_bucket = "s3://risk-clientele/clients", zipped = TRUE) {
 
     ## S3 Setup -----------------------------
     lid_company = company
     company_date = date
+
+    dest_path
+
+    if(is.null(dest_path)) {
+        dest_path = paste(lid_company, company_date, sep = '_')
+    }
+
+
+    ### Retrieve Bucket Data
 
     risk_bucket = "s3://risk-clientele/"
     clientele_s3 = aws.s3::get_bucket_df(risk_bucket) |> data.table::as.data.table()
     clientele_s3
     snapshot = company_date
 
-    ### INTERNALS  ----------------
+
+    ### Filter Company & Date
+
     if(snapshot == "latest") {
+
         extracted_part = clientele_s3$Key[grepl(lid_company, clientele_s3$Key)]
         snapshot_list = unique(regmatches(extracted_part, regexpr("[0-9]{4}-[0-9]{2}-[0-9]{2}", extracted_part)))
         company_date = max(snapshot_list)
+
+        cat('Lastest company_date available is', company_date)
+
     } else {
+
         company_date = snapshot
+
     }
 
-    company_date = paste(lid_company, company_date, 'resources/data/raw', data_type, '', sep = '/')
-    clientele_s3 = clientele_s3[Key %like% company_date][Key != company_date]
+    company_filter = paste(lid_company, company_date, sep = '/')
+    clientele_s3 = clientele_s3[Key %like% company_filter]
+    clientele_s3 = clientele_s3[Key != company_filter]
 
-    ### Download and Store  ----------------
-    dir.create(dest_path, showWarnings = FALSE)
-    lapply(clientele_s3$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path, risk_bucket = risk_bucket)
+
+    ### Download Files
+
+    if(data_type == "internal") {
+
+        string = paste(lid_company, company_date, 'resources/data/raw', data_type, '', sep = '/')
+        clientele_s3 = clientele_s3[Key %like% string]
+
+        dest_path = paste0(dest_path, '/', data_type)
+
+        dir.create(dest_path, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path, risk_bucket = risk_bucket)
+
+
+    } else if(data_type == "external") {
+
+        string = paste(lid_company, company_date, 'resources/data/raw', data_type, '', sep = '/')
+        clientele_s3 = clientele_s3[Key %like% string]
+
+        dest_path = paste0(dest_path, '/', data_type)
+
+        dir.create(dest_path, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path, risk_bucket = risk_bucket)
+
+
+    } else if(data_type == 'both') {
+
+        string_i = paste(lid_company, company_date, 'resources/data/raw/internal', '', sep = '/')
+        clientele_s3_i = clientele_s3[Key %like% string_i]
+
+        dest_path_i = paste0(dest_path, '/internal')
+
+        dir.create(dest_path_i, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3_i$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_i, risk_bucket = risk_bucket)
+
+
+        string_e = paste(lid_company, company_date, 'resources/data/raw/external', '', sep = '/')
+        clientele_s3_e = clientele_s3[Key %like% string_e]
+
+        dest_path_e = paste0(dest_path, '/external')
+
+        dir.create(dest_path_e, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3_e$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_e, risk_bucket = risk_bucket)
+
+
+    } else if(data_type == 'full') {
+
+        # Internals
+        string_i = paste(lid_company, company_date, 'resources/data/raw/internal', '', sep = '/')
+        clientele_s3_i = clientele_s3[Key %like% string_i]
+
+        dest_path_i = paste0(dest_path, '/internal')
+
+        dir.create(dest_path_i, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3_i$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_i, risk_bucket = risk_bucket)
+
+        # Externals
+        string_e = paste(lid_company, company_date, 'resources/data/raw/external', '', sep = '/')
+        clientele_s3_e = clientele_s3[Key %like% string_e]
+
+        dest_path_e = paste0(dest_path, '/external')
+
+        dir.create(dest_path_e, showWarnings = FALSE, recursive = TRUE)
+        lapply(clientele_s3_e$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_e, risk_bucket = risk_bucket)
+
+        # Docs
+        string_d = paste(lid_company, company_date, 'docs', '', sep = '/')
+        clientele_s3_d = clientele_s3[Key %like% string_d]
+
+        dest_path_d = paste0(dest_path, '/docs')
+        lapply(clientele_s3_d$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_d, risk_bucket = risk_bucket)
+
+        # Params
+        string_p = paste(lid_company, company_date, 'params', '', sep = '/')
+        clientele_s3_p = clientele_s3[Key %like% string_p]
+
+        dest_path_p = paste0(dest_path, '/params')
+        lapply(clientele_s3_p$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_p, risk_bucket = risk_bucket)
+
+
+    } else if(data_type == 'docs') {
+
+        # Docs
+        string_d = paste(lid_company, company_date, 'docs', '', sep = '/')
+        clientele_s3_d = clientele_s3[Key %like% string_d]
+
+        dest_path_d = paste0(dest_path, '/docs')
+        lapply(clientele_s3_d$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_d, risk_bucket = risk_bucket)
+
+
+    } else if(data_type == 'params') {
+
+        # Params
+        string_p = paste(lid_company, company_date, 'params', '', sep = '/')
+        clientele_s3_p = clientele_s3[Key %like% string_p]
+
+        dest_path_p = paste0(dest_path, '/params')
+        lapply(clientele_s3_p$Key, s3_retrieve_object, company_path = company_date, dest_path = dest_path_p, risk_bucket = risk_bucket)
+
+
+    } else {
+
+        paste(data_type, 'Not a valid data_type option.')
+
+    }
+
 
     ### ZIP  ----------------
 
     if(zipped) {
 
-        zip::zip(paste0(dest_path, "/", company, ".zip"), dest_path)
+        files_and_subfolders = list.files(dest_path, full.names = TRUE, recursive = TRUE)
+        zip_file = paste0(dest_path, '.zip')
+
+        # Create a zip file containing all the files and subfolders
+        utils::zip(zip_file, files_and_subfolders)
+
+        # Check if the zip file was created successfully
+        if (file.exists(zip_file)) {
+            cat("Zip file created successfully:", zip_file, "\n")
+        } else {
+            cat("Failed to create zip file:", zip_file, "\n")
+        }
 
         ### Clean  ----------------
         unlink(dest_path, recursive = TRUE)
 
     }
 
+    cat('Download Completed:', lid_company, 'for date', company_date)
+
 }
+
+
+
+
 
 
 
--- R/server_queries.R
+++ R/server_queries.R
@@ -10,17 +10,18 @@
 
 
 load_query = function(company, table) {
+
     query_template = paste0("
-    with users as (
+    with companies as (
         select *
-        from ca_myuser
-        where email like '%", company, "%'
+        from ca_mycompany
+        where lower(name) like '%", company, "%'
     ),
     ias as (
         -- invoice accounts
         select ia.*
         from invoice_invoiceaccount ia
-            join users on user_id = users.id
+            join companies on company_id = companies.id
     ),
     ics as (
         select ic.*
@@ -121,19 +122,19 @@ load_query = function(company, table) {
 
     uba as (
         select ub.*, b.name
-        from bankdatams_userbankaccount ub
-            join users on user_id = users.id
+        from bankdatams_bankaccount ub
+            join companies on company_id = companies.id
             left join bankdatams_bankinstitution b on ub.bank_institution_id = b.id
      ),
     ubt as (
         select ub.*, c.name as category
-        from bankdatams_userbanktransaction ub
+        from bankdatams_banktransaction ub
             join uba on ub.account_id = uba.id
             left join bankdatams_banktransactioncategorization c on ub.id = c.transaction_id
      ),
     ubb as (
         select ubb.*, b.name, b.iban
-            from bankdatams_userbankbalance ubb
+            from bankdatams_bankbalance ubb
             join uba b on ubb.account_id = b.id
      )
 
@@ -192,18 +193,160 @@ query_company_data = function(company, conn, BASE_PATH = "", verbose = TRUE) {
 #'
 
 query_company_detail = function(company, conn, BASE_PATH = "", verbose = TRUE) {
-    query = paste0("
-    SELECT a.email, b.* FROM ca_myuser a
-    LEFT JOIN userinfo_companyinfo b
-    ON a.id = b.user_id
-    WHERE LOWER(email) LIKE '%", company, "%';
-  ")
 
-    df = dbGetQuery(conn, query)
+    query = paste0("
+    SELECT id, name, vat_number, country, region, cap, city, address
+    FROM ca_mycompany;
+    ")
+
+    DTS = dbGetQuery(conn, query)
     final_name = paste0(BASE_PATH, 'company_detail', '.csv')
-    fwrite(df, final_name)
+    fwrite(DTS, final_name)
 
     if(verbose) {
         print(paste("Written file", final_name))
     }
 }
+
+
+#' Get Company Table
+#'
+#' This function retrieves from liqi-prod server the companies tables,
+#'
+#' @param conn A connection object representing the database connection.
+#' @param BASE_PATH A string representing the base path for the output file. Default is "".
+#' @param verbose A logical indicating whether to print messages. Default is TRUE.
+#' @importFrom DBI dbGetQuery
+#' @importFrom data.table fwrite
+#'
+#' @return Invisible NULL.
+#'
+#' @export
+#'
+
+get_company_table = function(conn, download = FALSE, BASE_PATH = "", verbose = TRUE) {
+
+    query = paste0("
+    SELECT id, name, vat_number, country, region, cap, city, address
+    FROM ca_mycompany;
+    ")
+
+    DTS = dbGetQuery(conn, query)
+
+    if(download) {
+
+        final_name = paste0(BASE_PATH, 'company_table', '.csv')
+        fwrite(DTS, final_name)
+
+        if(verbose) {
+            print(paste("Written file", final_name))
+        }
+
+    }
+
+    return(DTS)
+
+}
+
+
+#' SQL Open Connection
+#'
+#' This function connects to a SQL DB
+#'
+#' @param ssh the user
+#' @param url the complete string to the DB
+#'
+#' @return Open SQL Connection
+#'
+#' @export
+#'
+
+open_conn = function(ssh = 'alejandro@prod.bastion.liqi.it', url = '5432:prod-db-primary-20230426144532573700000011.cct8rb1f18rq.eu-south-1.rds.amazonaws.com:5432') {
+
+    command = paste0('ssh ', ssh, ' -L ', url)
+    system(paste("osascript -e 'tell app \"Terminal\" to do script \"", command, "\"'"))
+
+}
+
+#' SQL Close Connection
+#'
+#' This function connects to a SQL DB
+#'
+#' @return Close SQL Connection
+#'
+#' @export
+#'
+
+close_conn = function() {
+
+    Sys.sleep(1)
+
+    system("osascript -e 'tell app \"Terminal\" to close (every window whose name contains \"prod.bastion.liqi.it\")'")
+    Sys.sleep(1)
+
+    system("osascript -e 'tell application \"Terminal\" to activate'")
+    Sys.sleep(1)
+
+    system("osascript -e 'tell application \"System Events\" to tell process \"Terminal\" to keystroke \"Terminate\"'")
+    Sys.sleep(1)
+
+    system("osascript -e 'tell application \"System Events\" to tell process \"Terminal\" to keystroke return'")
+    Sys.sleep(1)
+
+    system("osascript -e 'tell application \"Terminal\" to quit'")
+
+}
+
+
+
+#' Interrogate italian companies DB
+#'
+#' This function connects and queries the SQLite DB
+#'
+#' @param conn A connection object representing the database connection.
+#' @param base sth.
+#' @param search_parameters query parameters.
+#'
+#' @return A data.table with the query result
+#'
+#' @export
+#'
+
+assembleQuery = function(conn, base, search_parameters) {
+
+    parameter_names = names(search_parameters)
+    partial_queries = ""
+
+    # Iterate over all the parameters to assemble the query
+    for(k in 1:length(parameter_names)){
+        # Check if the values are numeric or not
+        if(is.numeric(search_parameters[[parameter_names[k]]])){
+            values = paste(search_parameters[[parameter_names[k]]], collapse = ", ")
+        } else {
+            # Surround each value with single straight quotes if it's not numeric
+            values = paste("'", search_parameters[[parameter_names[k]]], "'", sep = "", collapse = ", ")
+        }
+
+        filter_k = paste(parameter_names[k], "IN (", values, ")")
+
+        # If there is more than 1 parameter, add an AND statement before the parameter
+        if(k > 1){
+            filter_k = paste("AND", filter_k)
+        }
+
+        partial_queries = paste(partial_queries, filter_k)
+    }
+
+    # Paste all together into a single query using a WHERE statement
+    final_query = paste(base, " WHERE", partial_queries)
+
+    # Print the assembled query to show how it looks like
+    print(final_query)
+
+    # Run the final query
+    result = dbGetQuery(conn, final_query)
+
+    # return the executed query
+    return(result)
+}
+
